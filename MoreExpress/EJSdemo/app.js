b77998d1c02fdbd27aff78f577a294a7f63a682d
Jonny <jonnymarx98@gmail.com>
2019-08-26 20:53:23 +0100

    some more ejs stuff

--- MoreExpress/EJSdemo/app.js
+++ MoreExpress/EJSdemo/app.js
@@ -1,15 +1,27 @@
 var express = require("express");
 var app = express();
 
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
 app.get("/", function(req, res){
-    res.render("home.ejs");
+    res.render("home");
 });
 
 app.get("/fallinlovewith/:thing", function(req, res){
     var thing = req.params.thing;
-    res.render("love.ejs", {thingVar: thing});
+    res.render("love", {thingVar: thing});
     // res.send("You fell in love with " + thing);
 });
+
+app.get("/posts", function(req, res){
+    var posts = [
+        {title: "Post 1", author: "susy"},
+        {title: "Post 2", author: "jonny"},
+        {title: "cake", author: "jayde"}
+    ];
+    res.render("posts",{posts: posts});
+})
 
 app.listen(3000, function(){
     console.log("serving on port 3000 bois!");
