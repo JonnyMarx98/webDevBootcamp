01f8c6702c309d9348a89f7a0a0dcbec55ad247a
Jonny <jonnymarx98@gmail.com>
2019-08-30 11:58:39 +0100

    blog site update

--- RESTfulBlogApp/app.js
+++ RESTfulBlogApp/app.js
@@ -1,14 +1,17 @@
-var bodyParser  = require("body-parser"),
-    mongoose    = require("mongoose"), 
-    express     = require("express"),
-    app         = express();
+var bodyParser      = require("body-parser"),
+    methodOverride  = require("method-override"),
+    mongoose        = require("mongoose"), 
+    express         = require("express"),
+    app             = express();
 
 
 // APP CONFIG 
 mongoose.connect("mongodb://localhost:27017/restful_blog_app", { useNewUrlParser: true });
+mongoose.set('useFindAndModify', false);
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
 
 // MONGOOSE MODEL CONFIG
 var blogSchema = mongoose.Schema({
@@ -27,10 +30,12 @@ var Blog = mongoose.model("Blog", blogSchema);
 
 // RESTFUL ROUTES
 
+
 app.get("/", (req, res) => {
     res.redirect("/blogs");
 })
 
+// INDEX ROUTE
 app.get("/blogs", (req, res) => {
     Blog.find({}, (err, blogs) => {
         if(err){
@@ -41,6 +46,57 @@ app.get("/blogs", (req, res) => {
     })
 })
 
+// NEW ROUTE
+app.get("/blogs/new", function(req, res){
+    res.render("new");
+})
+
+// CREATE ROUTE
+app.post("/blogs", (req, res) => {
+    // create blog
+    Blog.create(req.body.blog, (err, newBlog) => {
+        if(err){
+            res.render("new");
+        } else {
+            // redirect
+            res.redirect("/blogs");
+        }
+    })
+    
+})
+
+// SHOW ROUTE
+app.get("/blogs/:id", (req, res) => {
+    Blog.findById(req.params.id, (err, foundBlog) => {
+        if(err){
+            res.redirect("/blogs");
+        } else {
+            res.render("show", {blog: foundBlog});
+        }
+    });
+});
+
+// EDIT ROUTE
+app.get("/blogs/:id/edit", (req, res) => {
+    Blog.findById(req.params.id, (err, foundBlog) => {
+        if(err){
+            res.redirect("/blogs");
+        } else {
+            res.render("edit", {blog: foundBlog});
+        }
+    });
+});
+
+// UPDATE ROUTE
+app.put("/blogs/:id", (req, res) => {
+    Blog.findByIdAndUpdate(req.params.id, req.body.blog, (err, updatedBlog) => {
+        if(err){
+            res.redirect("/blogs");
+        } else {
+            res.redirect("/blogs/" + req.params.id);
+        }
+    })
+});
 
 app.listen(3000, () => {
     console.log("Server running bloggy");
