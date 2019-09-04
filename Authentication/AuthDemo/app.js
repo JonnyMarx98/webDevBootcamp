9d65311015b8fc3b5ac080c35cbe5e80ac977e05
Jonny <jonnymarx98@gmail.com>
2019-09-04 16:33:12 +0100

    Auth in progess

---
+++ Authentication/AuthDemo/app.js
@@ -0,0 +1,78 @@
+var express                 = require("express"),
+    mongoose                = require("mongoose"),
+    passport                = require("passport"),
+    bodyParser              = require("body-parser"),
+    LocalStrategy           = require("passport-local"),
+    User                    = require("./models/user"),
+    passportLocalMongoose   = require("passport-local-mongoose");
+
+mongoose.connect("mongodb://localhost:27017/auth_demo_app", { useNewUrlParser: true });
+
+var app = express();
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(require("express-session")({
+    secret: "Steven Cowie is Scottish",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+// ============
+// ROUTES
+// ============
+
+
+app.get("/", (req, res) => {
+    res.render("home");
+});
+
+app.get("/secret", (req, res) => {
+    res.render("secret");
+});
+
+// ===========
+// Auth Routes
+// ===========
+
+// Show sign up form
+app.get("/register", (req, res) => {
+    res.render("register");
+});
+
+// handle user sign up
+app.post("/register", (req, res) => {
+    User.register(new User({username: req.body.username}), 
+        req.body.password, (err, user) => {
+        if(err){
+            console.log(err);
+            return res.render("register");
+        }
+        passport.authenticate("local")(req, res, () => {
+            res.redirect("/secret");
+        })
+    })
+});
+
+// LOGIN ROUTES
+// register login form
+app.get("/login", (req, res) => {
+    res.render("login");
+});
+// login logic
+app.post("/login", passport.authenticate("local", {
+    successRedirect: "/secret",
+    failureRedirect: "/login"
+}),(req, res) => {
+});
+
+
+app.listen(3000, () => {
+    console.log("app running");
+});
